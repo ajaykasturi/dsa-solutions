29a66576e671b379fdd6f372703b8dd6905151c9
AjayKasturi <ajayreigns0@gmail.com>
2026-06-23 14:25:02 -0400

    feat: add nth node from end of linked list (2-methods)

---
+++ linked-list/011-nth-node-from-end-of-linked-list/Method1.java
@@ -0,0 +1,89 @@
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Method1 {
+    // Time: O(n)
+    public static void printNthNode(Node head, int n) {
+        if (n <= 0)
+            return;
+        int length = 0;
+        for (Node curr = head; curr != null; curr = curr.next)
+            length++;
+        if (length < n)
+            return;
+        int nth = (length - n + 1);
+        Node curr = head;
+        for (int i = 1; i < nth; i++) {
+            curr = curr.next;
+        }
+        System.out.printf("node %d is: %d\n", nth, curr.data);
+    }
+
+    public static void main(String[] args) {
+        test1();
+        test2();
+        test3();
+    }
+
+    public static void test1() {
+        System.out.println("test-1");
+        Node head = null;
+        head = insertEnd(head, 10);
+        head = insertEnd(head, 20);
+        head = insertEnd(head, 30);
+        head = insertEnd(head, 40);
+        head = insertEnd(head, 50);
+        printListIterative(head);
+        printNthNode(head, 2);
+    }
+
+    public static void test2() {
+        System.out.println("test-2");
+        Node head = null;
+        head = insertEnd(head, 10);
+        head = insertEnd(head, 20);
+        printListIterative(head);
+        printNthNode(head, 3);
+    }
+
+    public static void test3() {
+        System.out.println("test-3");
+        Node head = null;
+        head = insertEnd(head, 10);
+        head = insertEnd(head, 20);
+        head = insertEnd(head, 30);
+        printListIterative(head);
+        printNthNode(head, 3);
+    }
+
+    public static Node insertEnd(Node head, int data) {
+        Node temp = new Node(data);
+        if (head == null) { // when llist is empty
+            return temp;
+        }
+        Node curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = temp;
+        return head;
+    }
+
+    public static void printListIterative(Node head) {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+}
