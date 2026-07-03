8a0d896d4b39d0ed5e873324e58f3f1a47e8897b
AjayKasturi <ajayreigns0@gmail.com>
2026-07-03 10:56:03 -0400

    feat: add solution to find maximum in a binary tree

---
+++ tree/006-maximum-in-binary-tree/Solution.java
@@ -0,0 +1,50 @@
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int x) {
+        data = x;
+    }
+}
+
+public class Solution {
+    // Time: O(n)
+    // Aux. space: O(h); h=height of binary tree
+    public static int getMax(Node root) {
+        if (root == null)
+            return Integer.MIN_VALUE;
+        else
+            return Math.max(root.data, Math.max(getMax(root.left), getMax(root.right)));
+    }
+
+    public static void postOrder(Node root) {
+        if (root != null) {
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = null;
+        root = new Node(10);
+
+        // left subtree
+        root.left = new Node(20);
+
+        root.left.left = new Node(40);
+        root.left.right = new Node(50);
+
+        root.left.right.left = new Node(70);
+        root.left.right.right = new Node(80);
+
+        // right subtree
+        root.right = new Node(30);
+        root.right.right = new Node(60);
+
+        int res = getMax(root);
+        System.out.println(res);
+
+    }
+}
